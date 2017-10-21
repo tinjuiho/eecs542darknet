50e04918a850c4a8b8974e6ec744b2f80c19679b
Ray <tinjuiho@umich.edu>
2017-10-20 22:29:50 -0400

    find bugs2

--- detectImages.sh
+++ detectImages.sh
@@ -1,4 +1,4 @@
 rm -rf input.txt
 find testData -type f -maxdepth 1 | sort > input.txt
-./darknet detect cfg/yolo-voc.cfg yolo-voc.weights < input.txt
+./darknet detector test cfg/voc.data cfg/yolo-voc.cfg yolo-voc.weights < input.txt
 
