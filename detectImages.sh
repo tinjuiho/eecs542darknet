9cee43aedfac70d7283932a1904fde112611b363
Ray <tinjuiho@umich.edu>
2017-10-20 22:22:43 -0400

    find bugs

--- detectImages.sh
+++ detectImages.sh
@@ -1,4 +1,4 @@
 rm -rf input.txt
 find testData -type f -maxdepth 1 | sort > input.txt
-./darknet detect cfg/yolo.cfg yolo.weights < input.txt
+./darknet detect cfg/yolo-voc.cfg yolo-voc.weights < input.txt
 
